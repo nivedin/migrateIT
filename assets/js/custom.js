731b874a942f60307623c380f6e7d3b19f685400
Nivedin <nivedinp@gmail.com>
2020-10-07 21:13:28 +0530

    added faqBlur validation greenTick

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -52,13 +52,67 @@ formCheckBox2.addEventListener("click", () => {
 let proceedBtn = document.querySelector(".proceedBtn");
 let sourceServer = document.querySelector(".sourceServer");
 let destinationServer = document.querySelector(".destinationServer");
+let allSourceServerInputs = document.querySelectorAll(".sourceServer .form-group");
+let formContainer = document.querySelector(".formContainer");
 
-proceedBtn.addEventListener("click", () => {
-	sourceServer.classList.add("noDisplay");
-	destinationServer.classList.add("serverDetailAnim");
-	destinationServer.classList.remove("noDisplay");
 
+proceedBtn.addEventListener("click", (e) => {
+	// allSourceServerInputs.forEach(sourceInput => {
+	// 	let inputValue = sourceInput.value.length;
+	// 	if (inputValue != 0) {
+	// 		alert("dfd")
+	// 	} else {
+	// 		document.body.style.background = "#000";
+	// 	}
+
+	// })
+	e.preventDefault();
+	// sourceServer.classList.add("noDisplay");
+	// destinationServer.classList.add("serverDetailAnim");
+	// destinationServer.classList.remove("noDisplay");
+	var cpUsername = document.getElementById('myForm').Cpusername.value;
+	var cpPassword = document.getElementById('myForm').Cppassword.value;
+	var pathDirectory = document.getElementById('myForm').pathDirectory.value;
+	var serveIpHost = document.getElementById('myForm').serveIpHost.value;
+	if (cpUsername === "" || cpPassword === "" || pathDirectory === "" || serveIpHost === "") {
+		formContainer.classList.add("validateAnim");
+		formContainer.addEventListener("animationend", () => {
+			formContainer.classList.remove("validateAnim");
+		})
+
+		return false;
+	} else {
+		e.preventDefault();
+		sourceServer.classList.add("noDisplay");
+		destinationServer.classList.add("serverDetailAnim");
+		destinationServer.classList.remove("noDisplay");
+	}
 })
+
+
+// function validateForm() {
+// 	preventDefault();
+
+// 	console.log(cpUsername);
+// 	// alert(byTitle);
+// 	var cpPassword = document.getElementById('myForm').cpPassword.value;
+// 	// alert(byAuthor);
+// 	var pathDirectory = document.getElementById('myForm').pathDirectory.value;
+// 	//alert(startDate);
+// 	var serveIpHost = document.getElementById('myForm').serveIpHost.value;
+// 	//alert(endDate);       
+
+// 	if (!cpUsername && !cpPassword && !pathDirectory && !serveIpHost) {
+// 		alert("Enter at least one search parameter!");
+// 		return false;
+// 	} else {
+// 		e.preventDefault();
+// 		sourceServer.classList.add("noDisplay");
+// 		destinationServer.classList.add("serverDetailAnim");
+// 		destinationServer.classList.remove("noDisplay");
+// 	}
+// }
+
 
 
 let backBtn = document.querySelector(".backBtn");
@@ -74,10 +128,14 @@ backBtn.addEventListener("click", (e) => {
 let faqBtn = document.querySelector(".main-button-slider");
 let faqPopup = document.querySelector(".faqPopup");
 let popupCloseIco = document.querySelector(".closeIco");
+let faqPopBlur = document.querySelector(".faqPopBlur");
 
 faqBtn.addEventListener("click", () => {
+	faqPopBlur.classList.remove("noDisplay");
 	faqPopup.classList.remove("noDisplay");
 	popupCloseIco.addEventListener("click", () => {
 		faqPopup.classList.add("noDisplay");
+		faqPopBlur.classList.add("noDisplay");
+
 	})
 })
